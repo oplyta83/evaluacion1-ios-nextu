8fa307f7185fd2c5df47d77f5e83c3be6f732374
sebastian Abarzua <sabarzua@outlook.com>
2018-08-08 21:02:13 -0400

    se agregar el redireccionamiento al navegador web nativo de los dipositivos

---
+++ evaluacion1-ios-nextu/evaluacion1-ios-nextu/ConditionalViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ConditionalViewController.swift
+//  evaluacion1-ios-nextu
+//
+//  Created by Sebastian Abarzua on 08-08-18.
+//  Copyright © 2018 nextu.com. All rights reserved.
+//
+
+import UIKit
+
+class ConditionalViewController: UIViewController {
+
+    
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var txtDefinition: UITextView!
+    
+    let urlEnglish : URL = URL(string: "https://en.wikipedia.org/wiki/Abstraction_(computer_science)")!
+    let urlSpanish : URL = URL(string: "https://es.wikipedia.org/wiki/Abstracci%C3%B3n_(inform%C3%A1tica)")!
+    
+    let Name : String = "Abtraction"
+    
+    let DefinitionEnglish : String = """
+        
+    """
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+ 
+    
+    @IBAction func btnWikipedia(_ sender: Any) {
+        
+        UIApplication.shared.open(urlSpanish, options: [ : ], completionHandler: nil)
+        
+    }
+    
+    
+}
